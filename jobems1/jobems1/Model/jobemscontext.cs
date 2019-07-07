2a120fbd9cc228ef3ceae8be1e02c8794736da47
bookydoosh <52623997+bookydoosh@users.noreply.github.com>
2019-07-07 22:42:36 +1200

    Add files via upload

---
+++ jobems1/jobems1/Model/jobemscontext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace jobems1.Model
+{
+    public partial class jobemscontext : DbContext
+    {
+        public jobemscontext()
+        {
+        }
+
+        public jobemscontext(DbContextOptions<jobemscontext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employee { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=tcp:msajobems.database.windows.net,1433;Initial Catalog=msajobems;Persist Security Info=False;User ID=mraj741;Password=Mohnishmsaassignment!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.FirstName).IsUnicode(false);
+
+                entity.Property(e => e.JobTitle).IsUnicode(false);
+
+                entity.Property(e => e.LastName).IsUnicode(false);
+
+                entity.Property(e => e.PhoneNumber).IsUnicode(false);
+            });
+        }
+    }
+}
